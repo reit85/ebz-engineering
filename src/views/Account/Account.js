7dae801cb684431ecc4684a622ef3fef9814b556
Reit <Alexander.Reit@ebz-group.com>
2020-01-17 13:24:32 +0100

    Updated account view

--- src/views/Account/Account.js
+++ src/views/Account/Account.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
 
-import { AccountProfile, AccountDetails } from './components';
-import Settings from './Settings/Settings'
+import { AccountProfile, AccountDetails, Notifications, Password } from './components';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,30 +15,20 @@ const Account = () => {
 
   return (
     <div className={classes.root}>
-      <Grid
-        container
-        spacing={4}
-      >
-        <Grid
-          item
-          lg={4}
-          md={6}
-          xl={4}
-          xs={12}
-        >
-          <AccountProfile />
-        </Grid>
-        <Grid
-          item
-          lg={8}
-          md={6}
-          xl={8}
-          xs={12}
-        >
+      <Grid container spacing={4}>
+        <Grid item lg={8} md={6} xl={8} xs={12}>
           <AccountDetails />
         </Grid>
+        <Grid item lg={4} md={6} xl={4} xs={12}>
+          <AccountProfile />
+        </Grid>
+        <Grid item lg={8} md={6} xl={8} xs={12}>
+          <Notifications />
+        </Grid>
+        <Grid item lg={4} md={6} xl={4} xs={12}>
+          <Password />
+        </Grid>
       </Grid>
-      <Settings />
     </div>
   );
 };
--- src/views/Account/components/index.js
+++ src/views/Account/components/index.js
@@ -1,2 +1,4 @@
 export { default as AccountDetails } from './AccountDetails';
 export { default as AccountProfile } from './AccountProfile';
+export { default as Notifications } from './Notifications';
+export { default as Password } from './Password';
